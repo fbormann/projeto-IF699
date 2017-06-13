c06ff1df1797ca4b60e75e9edb81bc505e89870b
Felipe Bormann <felipe.bormann@gmail.com>
2017-06-13 14:37:52 -0300

    added beta array calculation correctly on trainning

--- elm.py
+++ elm.py
@@ -2,11 +2,13 @@ import numpy as np
 import random
 from neuron import Neuron
 
+from numpy.linalg import inv
+
 class ELM(object):
 	"""docstring for ELM"""
 	def __init__(self, input_size, output_layer_size):
 		super(ELM, self).__init__()
-		self.H = np.empty((0,0))
+		self.H = None
 		self.bias = []
 		self.input_size = input_size
 		self.hidden_layer = []
@@ -18,13 +20,21 @@ class ELM(object):
 
 	def train(self, input, **kwargs):
 		outputs = []
+		
 		for i in range(len(self.hidden_layer)):
 			outputs.append(self.hidden_layer[i].activate( self.input_weights[i].dot(input) +  self.bias[i]))
 		#print(outputs)
-		self.H  = np.vstack([self.H, np.array(outputs)]) #add row to H matrix
+		
+		self.H = np.vstack([self.H, np.array(outputs)]) #add row to H matrix
+		
+	
+		#print(self.H.shape)
 
 
-
+	def update_beta(self, output):
+		output = np.array(output)
+		alpha = 50*0.001
+		self.output_weights = inv((self.H.T.dot(self.H)) + np.identity(self.H.shape[1])*alpha).dot(self.H.T).dot(output.T)
 		
 	def predict(self, input):
 		pass
@@ -34,7 +44,7 @@ class ELM(object):
 		for i in range(amount):
 			self.hidden_layer.append(Neuron(kind))
 			self.set_correspondent_weight_and_bias(kind)
-		self.H = np.empty((1, len(self.hidden_layer)))
+		self.H = np.empty((0, len(self.hidden_layer)))
 
 	def set_correspondent_weight_and_bias(self, kind):
 		if kind == 'linear':
--- main.py
+++ main.py
@@ -14,9 +14,16 @@ neural_network = ELM(input_size = 9, output_layer_size = 2)
 neural_network.add_neuron(9, "linear")
 neural_network.add_neuron(2, "sigmoid")
 
+output_classes = []
+print(len(train))
 for item in train.values:
-	
+	#item[:len(item)-1]
 	neural_network.train(item[:len(item)-1])
+	
+	output_classes.append(item[len(item)-1])
+neural_network.update_beta(output_classes) #create output_weights
+
+
 if debug:
 	print("checking correctness of shapes:")
 	print(neural_network.input_weights[0].shape)
